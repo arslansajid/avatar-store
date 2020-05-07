56af3a9a4b8426f7534a9bdf3e41b039f4c44cc5
Arslan Sajid <arslansajid951@gmail.com>
2020-05-08 01:53:14 +0500

    config.js added

---
+++ Config.js
@@ -0,0 +1,12 @@
+const Config = {
+  DEV_MODE: false,
+
+  BASE_APP_URL: 'http://localhost:3001/api',
+
+  API_VERSION: 'v1',
+};
+
+// Config.API_END_POINT = Config.BASE_APP_URL + Config.API_VERSION;
+Config.API_END_POINT = Config.BASE_APP_URL;
+
+export default Config;
