dd9da1450b97e66a50d52d205e6889a59b61015c
Arslan Sajid <arslansajid951@gmail.com>
2020-05-09 17:10:40 +0500

    Added user info on my account page

--- src/pages/myaccount.js
+++ src/pages/myaccount.js
@@ -1,41 +1,61 @@
-import React, {useState, useEffect, useContext} from 'react'
-import {navigate} from 'gatsby'
+import React, { useState, useEffect, useContext } from 'react'
+import { navigate } from 'gatsby'
 import SEO from '../components/SEO'
 import OrderItemList from '../components/OrderItemList'
 import Layout from '../components/Layout'
 import AuthContext from '../components/Context/AuthContext'
+import axios from "axios";
+import Config from "../Api/Config"
 
-import {getOrders} from '../../lib/moltin'
+import { getOrders } from '../../lib/moltin'
+import { getUserProfile } from "../Api";
+import { Card, Image } from 'semantic-ui-react'
 
-const MyAccount = ({location}) => {
+const MyAccount = ({ location }) => {
   const [loading, setLoading] = useState(true)
   const [orders, setOrders] = useState([])
   const [included, setIncluded] = useState([])
   const [meta, setMeta] = useState({})
-  const {token} = useContext(AuthContext)
+  const [user, setUser] = useState(null)
+  const { token } = useContext(AuthContext)
 
   useEffect(() => {
     if (!token) {
       navigate('/login/')
-    }
-    getOrders(token)
-      .then(({data, meta, included}) => {
-        const orders = data.map(order => ({
-          ...order,
-        }))
+    } else {
+      axios.get(`${Config.API_END_POINT}/user/me`) //Instance is not used here intentionally
+      .then((res) => {
+        console.log("USER DATA !", res.data)
+        setUser(res.data)
         setLoading(false)
-        setMeta(meta)
-        setOrders(orders)
-        setIncluded(included)
       })
-      .catch(error => {
-        console.log(error)
+      .catch((error) => {
+        // console.log("ERROR FETCHING USER DATA !", error.data.message)
       })
+    }
+    // getOrders(token)
+    //   .then(({data, meta, included}) => {
+    //     const orders = data.map(order => ({
+    //       ...order,
+    //     }))
+    //     setLoading(false)
+    //     setMeta(meta)
+    //     setOrders(orders)
+    //     setIncluded(included)
+    //   })
+    //   .catch(error => {
+    //     console.log(error)
+    //   })
   }, [token])
 
   return (
     <Layout location={location}>
       <SEO title="My Account" />
+      {!!user && (
+        <h3>
+          Welcome {user.email}!
+        </h3>
+      )}
       <OrderItemList
         meta={meta}
         orders={orders}
