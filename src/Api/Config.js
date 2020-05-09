0360685491d814b63ed7f2da3b18af8b538e3e49
Arslan Sajid <arslansajid951@gmail.com>
2020-05-09 17:14:30 +0500

    config files path changed

---
+++ src/Api/Config.js
@@ -0,0 +1,13 @@
+const Config = {
+  DEV_MODE: false,
+
+  BASE_APP_URL: 'http://localhost:3001/api',
+  // BASE_APP_URL: 'https://familyportrait-be.herokuapp.com/api',
+
+  API_VERSION: 'v1',
+};
+
+// Config.API_END_POINT = Config.BASE_APP_URL + Config.API_VERSION;
+Config.API_END_POINT = Config.BASE_APP_URL;
+
+export default Config;
