b2c48680417d4b90ebdd822f51ba9c1ee1205cd1
Arslan Sajid <arslansajid951@gmail.com>
2020-05-20 15:35:05 +0500

    Layput updated

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import Headroom from 'react-headroom'
-import {Container} from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import Footer from '../Footer'
 import Header from '../Header'
 import 'semantic-ui-css/semantic.min.css'
+import axios from "axios";
+import Cookie from 'js-cookie';
 
-const Layout = ({location, children}) => (
-  <>
-    <Headroom
-      upTolerance={10}
-      downTolerance={10}
-      style={{zIndex: '20', height: '6.5em'}}
-    >
-      <Header location={location} />
-    </Headroom>
-    <Container style={styles.container}>{children}</Container>
-    <Footer />
-  </>
-)
-
-const styles = {
-  container: {
-  }
+const token = Cookie.get('family_portrait_access_token');
+if (token) {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 }
-export default Layout;
+
+  const Layout = ({ location, children }) => {
+    return (
+      <>
+        <Headroom
+          upTolerance={10}
+          downTolerance={10}
+          style={{ zIndex: '20', height: '6.5em' }}
+        >
+          <Header location={location} />
+        </Headroom>
+        <Container style={styles.container}>{children}</Container>
+        <Footer />
+      </>
+    )
+  }
+
+  const styles = {
+    container: {
+    }
+  }
+  export default Layout;
