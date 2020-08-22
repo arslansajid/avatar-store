10dff303836744f0f9329d73547bd94931eac24d
Arslan Sajid <arslansajid951@gmail.com>
2020-08-22 23:37:18 +0500

    my account call issue fixed

--- src/Api/api.config.js
+++ src/Api/api.config.js
@@ -2,18 +2,20 @@ import Config from './Config';
 import axios from 'axios';
 import Cookie from 'js-cookie';
 
-// const token = Cookie.get('family_portrait_access_token');
+const token = Cookie.get('family_portrait_access_token');
 // if(!!token) {
 //   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   axiosInstance.defaults.headers['Authorization'] = `${token}`; 
 // }
 
 const axiosInstance = axios.create({
   baseURL: Config.API_END_POINT,
   timeout: 30000, // 30 secs.
   withCredentials: true,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // },
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization' : !!token ? `Bearer ${token}` : '',
+  }, 
 });
 
 export default axiosInstance;
